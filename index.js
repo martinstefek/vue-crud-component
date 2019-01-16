140b68e5ff35a8e4dd752b63b016bf47bb24159e
= <=>
2019-01-16 22:15:14 +0100

    REMOVE

--- index.js
+++
@@ -1,9 +0,0 @@
-import CrudComponent from './components/Crud'
-import VueSweetalert2 from 'vue-sweetalert2'
-import Notifications from 'vue-notification'
-
-exports.install = function(Vue, options = {}) {
-    Vue.use(VueSweetalert2)
-    Vue.use(Notifications)
-    Vue.component(options.name || 'crud-component', CrudComponent)
-}
